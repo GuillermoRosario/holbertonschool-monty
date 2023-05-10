6fc1aa06e74ae021fa52b875d962838e1425c6b1
GuillermoRosario <guillermorosario628@gmail.com>
2023-05-09 21:58:00 -0400

    push handle error

--- monty.h
+++ monty.h
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 /**
 (/)
