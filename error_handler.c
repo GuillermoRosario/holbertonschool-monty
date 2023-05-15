2a6c8cded7449d4a811aeb5f0c5fa1d89a391f4e
PhantomMik3 <miguelforex77@gmail.com>
2023-05-15 20:54:44 +0000

    error handler 1 fixed

--- error_handler.c
+++ error_handler.c
@@ -1,7 +1,7 @@
 #include"monty.h"
 /**
  * error_arguments -Main entry
- *Description: If user doesnt give file or more than valid arguments
+ * Description: If user doesnt give file or more than valid arguments
  * Return: void
  **/
 void error_arguments(void)
@@ -11,47 +11,33 @@ void error_arguments(void)
 }
 
 /**
- * open_error -Main entry
- *Description: print message if its not possible to open the file
- * @argv: arguments received by main, ni this case te filename
- * Return: void
- **/
-void open_error(char **argv)
-{
-	char *filename = argv[1];
-
-	fprintf(stderr, "Error: Can't open file %s\n", filename);
-	exit(EXIT_FAILURE);
-}
-
-/**
  * invalidInstruction_error -Main entry
- *Description: print message if le to open the filehas an invalid instruction
- * @invInstruction: instruction
- * @line: line
+ * Description: print message if le to open the filehas an invalid instruction
+ * @op_code: instruction
+ * @line_number: line
  * Return: void
  **/
 void invalidInstruction_error(char *op_code, unsigned int line_number)
 {
-	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, op_code);
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, op_code);
 	exit(EXIT_FAILURE);
 }
 
 /**
  * not_int_err -Main entry
- *Description: print message if parameter received is not integer
- * @line: line
+ * Description: print message if parameter received is not integer
+ * @line_number: line
  * Return: void
  **/
 void not_int_err(unsigned int line_number)
 {
-	fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	fprintf(stderr, "L%d: usage: push integer\n", line_number);
 	exit(EXIT_FAILURE);
 }
 
 /**
  * malloc_error -Main entry
- *Description: print message if cant malloc anymore
+ * Description: print message if cant malloc anymore
  * Return: void
  **/
 void malloc_error(void)
@@ -59,3 +45,4 @@ void malloc_error(void)
 	fprintf(stderr, "Error: malloc failed\n");
 	exit(EXIT_FAILURE);
 }
+
